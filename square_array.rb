a83953c0b1217ed4f6b917f7f2a7d51d3e412674
Warren Loy Harper II <warrenharper2@att.net>
2019-08-15 00:21:50 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,5 @@
 def square_array(array)
-  array = [1,2,3]
-  array.each do |square|
-    square = array**2
-    puts square
+  new_array = []
+  array.each{|a| new_array.push(a*a)}
+  return new_array
 end
